819d51ae8f822b17e8d5f0a237e798b3fd59303e
pedro <pedperiba@alu.edu.gva.es>
2026-06-05 18:06:47 +0200

    feat: update Header component with logo link and set Spanish as default language in index.html

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,27 +10,44 @@ const routes = [
     path: '/',
     name: 'Home',
     component: HomeView,
+    meta: {
+      title: 'Inicio',
+    },
   },
   {
     path: '/proyectos',
     name: 'Projects',
     component: ProjectsView,
+    meta: {
+      title: 'Proyectos',
+    },
   },
   {
     path: '/sobre-mi',
     name: 'About',
     component: AboutView,
+    meta: {
+      title: 'Sobre mi',
+    },
   },
   {
     path: '/patron',
     name: 'Patron',
-    component: ThemeSwitcher
-  }
+    component: ThemeSwitcher,
+    meta: {
+      title: 'Patron web',
+    },
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `Porfolio - ${<string>to.meta.title}` || 'Mi app'
+  next()
 })
 
 export default router
