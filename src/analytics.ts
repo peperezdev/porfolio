6ec214a9a51f1574b20dd13c3986c7f9b65b0806
pedro <pedperiba@alu.edu.gva.es>
2026-06-21 00:28:48 +0200

    fix: update API_URL for analytics and enable logging

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,4 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3002'
+// const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3002'
+const API_URL = import.meta.env.VITE_API_URL || 'https://porfoliobackend.peperez.es'
 
 console.log('API_URL:', API_URL)
 
